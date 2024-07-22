72f8c43768f083061aae2778690f2504a79e7f2b
dm-lalwani <mr.d.m.lalwani@gmail.com>
2024-07-22 16:35:32 +0530

    Disbursement & Repayment API's updated

--- src/components/NewUser/Repayments.jsx
+++ src/components/NewUser/Repayments.jsx
@@ -77,7 +77,7 @@ const Repayment = () => {
     try {
       const token = localStorage.getItem("authToken");
       const data = await fetch(
-        "https://api-test.lmscarbon.com/carbon-offers-service/lmscarbon/api/v1/borrowers/" +
+        "https://api-dev.lmscarbon.com/carbon-offers-service/lmscarbon/api/v1/borrowers/" +
           userID +
           "/loans/closure",
         {
