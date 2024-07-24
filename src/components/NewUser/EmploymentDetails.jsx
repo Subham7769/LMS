391c4fad264c07e3a6933f9811c121653979f744
dm-lalwani <mr.d.m.lalwani@gmail.com>
2024-07-24 12:19:05 +0530

    Employment Details optimized

--- src/components/NewUser/EmploymentDetails.jsx
+++ src/components/NewUser/EmploymentDetails.jsx
@@ -6,6 +6,7 @@ import { Passed } from "../Toasts";
 import LoadingState from "../LoadingState/LoadingState";
 import InputNumber from "../Common/InputNumber/InputNumber";
 import InputText from "../Common/InputText/InputText";
+import Button from "../Common/Button/Button";
 
 function EmploymentDetails() {
   // Params Data, API urls and Auth Token
@@ -28,7 +29,7 @@ function EmploymentDetails() {
     establishmentActivity: "",
   });
 
-  const handleInputChange = (e) => {
+  const handleChange = (e) => {
     const { name, value, checked, type } = e.target;
     if (type === "checkbox") {
       setFormData((prevState) => ({ ...prevState, [name]: checked }));
@@ -122,106 +123,77 @@ function EmploymentDetails() {
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
-      <div>
-        <h2 className="mb-5 px-3 py-2 hover:bg-gray-100 rounded-md w-1/5 cursor-pointer">
-          <b>Employment Details</b>
-        </h2>
-        <div className="w-full mx-auto bg-white p-6 shadow-md rounded-xl border border-red-600">
-          <form className="grid grid-cols-1 md:grid-cols-4 gap-5">
-            {/* Full Name */}
-            <div className="col-span-1">
-              <InputText
-                labelName="Full Name"
-                inputName="fullName"
-                placeHolder="John Doe"
-                inputValue={formData.fullName}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-
-            {/* Basic Wage */}
-            <div className="col-span-1">
-              <InputNumber
-                labelName="Basic Wage"
-                inputName="basicWage"
-                placeHolder="4000"
-                inputValue={formData.basicWage}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-
-            {/* Housing Allowance */}
-            <div className="col-span-1">
-              <InputNumber
-                labelName="Housing Allowance"
-                inputName="housingAllowance"
-                placeHolder="1000"
-                inputValue={formData.housingAllowance}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-
-            {/* Employer Name */}
-            <div className="col-span-1">
-              <InputText
-                labelName="Employer Name"
-                inputName="employerName"
-                placeHolder="John"
-                inputValue={formData.employerName}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-
-            {/* Working Months */}
-            <div className="col-span-1">
-              <InputNumber
-                labelName="Working Months"
-                inputName="workingMonths"
-                placeHolder="24"
-                inputValue={formData.workingMonths}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-
-            {/* Employment Status */}
-            <div className="col-span-1">
-              <InputText
-                labelName="Employment Status"
-                inputName="employmentStatus"
-                placeHolder="Unemployed"
-                inputValue={formData.employmentStatus}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-
-            {/* Establishment Activity */}
-            <div className="col-span-1">
-              <InputText
-                labelName="Establishment Activity"
-                inputName="establishmentActivity"
-                placeHolder="Developer"
-                inputValue={formData.establishmentActivity}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-          </form>
-          <div className="flex items-center justify-end gap-4 mt-4">
-            <button
-              type="button"
-              onClick={updateData}
-              className="inline-flex items-center gap-x-1.5 mt-3 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-44 justify-center"
-            >
-              <CheckCircleIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
-              Update
-            </button>
-          </div>
+      <h2 className="mb-5 px-3 py-2 hover:bg-gray-100 rounded-md w-1/5 cursor-pointer">
+        <b>Employment Details</b>
+      </h2>
+      <div className="w-full mx-auto bg-white p-6 shadow-md rounded-xl border border-red-600">
+        <form className="grid grid-cols-1 md:grid-cols-4 gap-5">
+          {/* Full Name */}
+            <InputText
+              labelName="Full Name"
+              inputName="fullName"
+              placeHolder="John Doe"
+              inputValue={formData.fullName}
+              onChange={handleChange}
+              required
+            />
+          {/* Basic Wage */}
+            <InputNumber
+              labelName="Basic Wage"
+              inputName="basicWage"
+              placeHolder="4000"
+              inputValue={formData.basicWage}
+              onChange={handleChange}
+              required
+            />
+          {/* Housing Allowance */}
+            <InputNumber
+              labelName="Housing Allowance"
+              inputName="housingAllowance"
+              placeHolder="1000"
+              inputValue={formData.housingAllowance}
+              onChange={handleChange}
+              required
+            />
+          {/* Employer Name */}
+            <InputText
+              labelName="Employer Name"
+              inputName="employerName"
+              placeHolder="John"
+              inputValue={formData.employerName}
+              onChange={handleChange}
+              required
+            />
+          {/* Working Months */}
+            <InputNumber
+              labelName="Working Months"
+              inputName="workingMonths"
+              placeHolder="24"
+              inputValue={formData.workingMonths}
+              onChange={handleChange}
+              required
+            />
+          {/* Employment Status */}
+            <InputText
+              labelName="Employment Status"
+              inputName="employmentStatus"
+              placeHolder="Unemployed"
+              inputValue={formData.employmentStatus}
+              onChange={handleChange}
+              required
+            />
+          {/* Establishment Activity */}
+            <InputText
+              labelName="Establishment Activity"
+              inputName="establishmentActivity"
+              placeHolder="Developer"
+              inputValue={formData.establishmentActivity}
+              onChange={handleChange}
+              required
+            />
+        </form>
+        <div className="flex items-center justify-end gap-4 mt-4">
+          <Button buttonIcon={CheckCircleIcon} buttonName={"Update"} onClick={updateData} rectangle={true}/>
         </div>
       </div>
     </>
