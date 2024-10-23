893e560d7f91fdaa9871af7ed9a3d5239bc7fb47
dm-lalwani <mr.d.m.lalwani@gmail.com>
2024-10-23 16:57:17 +0530

    HomePage ProductList Table Navigation updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
 import ListTable from "../components/Common/ListTable/ListTable";
-import { HomeStats, HeaderList, ProductList } from '../data/HomeData';
-import StatContainer from '../components/Common/StatContainer/StatContainer';
-
+import { HomeStats } from "../data/HomeData";
+import StatContainer from "../components/Common/StatContainer/StatContainer";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
-    return (
-        <>
-            <StatContainer stats={HomeStats} />
-            <ListTable 
-            ListName={"Product List"} 
-            ListHeader={HeaderList} 
-            ListItem={ProductList}
-            Searchable={true}
-            Sortable={true} // New prop to enable/disable sorting
-            />
-        </>
-    )
-}
+  const { HeaderList, ProductList } = useSelector(
+    (state) => state.product.productStatsData
+  );
+  return (
+    <>
+      <StatContainer stats={HomeStats} />
+      <ListTable
+        ListName={"Product List"}
+        ListHeader={HeaderList}
+        ListItem={ProductList}
+        Searchable={true}
+        Sortable={true} // New prop to enable/disable sorting
+      />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
