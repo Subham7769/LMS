960def1feaf7ac68ebabee86a272d5b6a94c7337
dm-lalwani <mr.d.m.lalwani@gmail.com>
2025-01-17 18:31:18 +0530

    Search added in dynamic RAC, approve repayment UI updated in test

---
+++ src/components/TestComponent/Los/ApproveRepaymentTest.jsx
@@ -0,0 +1,310 @@
+import React, { useEffect, useState } from "react";
+import ExpandableTable from "../../Common/ExpandableTable/ExpandableTable";
+import ContainerTile from "../../Common/ContainerTile/ContainerTile";
+import Button from "../../Common/Button/Button";
+import InputSelect from "../../Common/InputSelect/InputSelect";
+import InputText from "../../Common/InputText/InputText";
+import { ApproveRepaymentColumns } from "../../../data/LosData";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getRepayments,
+  approveRepayment,
+  rejectRepayment,
+  fetchRepaymentByField,
+} from "../../../redux/Slices/personalRepaymentsSlice";
+import { getFullLoanDetails } from "../../../redux/Slices/personalLoansSlice";
+import Pagination from "../../Common/Pagination/Pagination";
+import {
+  CurrencyDollarIcon,
+  ClipboardDocumentListIcon,
+  UserIcon,
+  ClockIcon,
+  CalendarDaysIcon,
+} from "@heroicons/react/24/outline";
+import { toast } from "react-toastify";
+import { FiCheckCircle, FiInfo, FiXCircle } from "react-icons/fi";
+import FullLoanDetailModal from "./FullLoanDetailModal";
+import { convertDate } from "../../../utils/convertDate";
+
+const paymentsData = [
+  {
+    paymentDate: "16 Dec 2024",
+    paymentType: "Regular Payment",
+    paymentAmount: "$445.00",
+    paymentStatus: "Completed",
+  },
+  {
+    paymentDate: "17 Dec 2024",
+    paymentType: "Regular Payment",
+    paymentAmount: "$445.00",
+    paymentStatus: "Completed",
+  },
+  {
+    paymentDate: "18 Dec 2024",
+    paymentType: "Regular Payment",
+    paymentAmount: "$445.00",
+    paymentStatus: "Completed",
+  },
+];
+
+function transformData(inputArray) {
+  return inputArray.map((item) => ({
+    ...item,
+    collectionDate: convertDate(item?.collectionDate),
+    aging: "4 Days"
+  }));
+}
+
+const ApproveRepaymentTest = () => {
+  const dispatch = useDispatch();
+  const {
+    approveRepaymentData,
+    approveRepaymentTotalElements,
+    loading,
+    error,
+  } = useSelector((state) => state.personalRepayments);
+  const { fullLoanDetails } = useSelector((state) => state.personalLoans);
+  const loading2 = useSelector((state) => state.personalLoans.loading);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showLoanModal, setShowLoanModal] = useState(false);
+  // Pagination state & Functionality
+  const [pageSize, setPageSize] = useState(10);
+  const [searchValue, setSearchValue] = useState("");
+  const [searchBy, setSearchBy] = useState("");
+
+  const dispatcherFunction = (currentPage, pageSize) => {
+    dispatch(getRepayments({ pageNumber: currentPage, pageSize: pageSize }));
+  };
+
+  const handleApprove = async (transactionId) => {
+    await dispatch(approveRepayment({ transactionId })).unwrap();
+    dispatch(getRepayments({ pageSize: pageSize, pageNumber: 0 }));
+  };
+
+  const handleReject = async (transactionId) => {
+    await dispatch(rejectRepayment({ transactionId })).unwrap();
+    dispatch(getRepayments({ pageSize: pageSize, pageNumber: 0 }));
+  };
+
+  const transformedRepaymentData = transformData(approveRepaymentData);
+
+  const copyToClipboard = async (transactionId) => {
+    try {
+      await navigator.clipboard.writeText(transactionId);
+      toast.success("ID was copied successfully!");
+    } catch (err) {
+      toast.error("The ID was not copied!");
+    }
+  };
+
+  const renderExpandedRow = (rowData) => (
+    <div className="text-sm text-gray-600 border-y-2 py-5">
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-white shadow p-3 rounded">
+          <div className="text-lg font-semibold flex gap-2 items-center mb-4">
+            <CurrencyDollarIcon className="-ml-0.5 h-5 w-5 text-blue-500" />
+            Payment Details
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Loan Product</div>
+            <div className="font-semibold">Payroll Backed Loan</div>
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Transaction ID</div>
+            <div className="flex">
+              <div className="font-semibold whitespace-nowrap overflow-hidden text-ellipsis w-[100px]">
+                {rowData.transactionId}
+              </div>
+              <ClipboardDocumentListIcon
+                onClick={() => copyToClipboard(rowData.transactionId)}
+                className="-ml-0.5 h-5 w-5 cursor-pointer"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Original Loan Amount</div>
+            <div className="font-semibold">$5000</div>
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Outstanding Balance</div>
+            <div className="font-semibold">$3560</div>
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Due Date</div>
+            <div className="font-semibold">20 Jan 2025</div>
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Status</div>
+            <div className="text-xs bg-black text-white py-1 px-2 rounded">
+              On time
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow p-3 rounded">
+          <div className="text-lg font-semibold flex gap-2 items-center mb-4">
+            <UserIcon className="-ml-0.5 h-5 w-5 text-blue-500" />
+            Borrower Profile
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Name</div>
+            <div className="text-gray-700">John Doe</div>
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">User ID</div>
+            <div className="text-gray-700">{rowData.userId}</div>
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Email</div>
+            <div className="text-gray-700">john.doe@example.com</div>
+          </div>
+          <div className="flex justify-between mb-2">
+            <div className="text-gray-500">Phone</div>
+            <div className="text-gray-700">+1 (555) 123-4567</div>
+          </div>
+        </div>
+        <div className="bg-white shadow p-3 rounded">
+          <div className="text-lg font-semibold flex gap-2 items-center mb-4">
+            <ClockIcon className="-ml-0.5 h-5 w-5 text-blue-500" />
+            Recent Payments
+          </div>
+          {paymentsData.map((payment) => (
+            <div className="flex justify-between mb-2 border-b border-gray-300 pb-3">
+              <div>
+                <div className="text-black font-semibold">
+                  {payment.paymentDate}
+                </div>
+                <div className="font-light">{payment.paymentType}</div>
+              </div>
+              <div className="text-right">
+                <div className="text-black font-semibold">
+                  {payment.paymentAmount}
+                </div>
+                <div className="border border-gray-200 rounded shadow py-1 px-2 text-xs text-black">
+                  {payment.paymentStatus}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex justify-end gap-2 px-5 mt-5">
+        <button
+          onClick={() => handleFullLoanDetails(rowData.loan, rowData.userId)}
+          className="flex gap-x-1.5 items-center px-2.5 py-2 bg-white shadow-md text-blue-600 rounded-md hover:shadow transition-colors"
+        >
+          <CalendarDaysIcon className="-ml-0.5 h-5 w-5" />
+          View EMI Schedule
+        </button>
+        <button
+          onClick={() => handleReject(rowData)}
+          className="flex gap-x-1.5 items-center px-2.5 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition-colors"
+        >
+          <FiXCircle className="-ml-0.5 h-5 w-5" />
+          Reject
+        </button>
+        <button
+          onClick={() => handleApprove(rowData)}
+          className="flex gap-x-1.5 items-center px-2.5 py-2 bg-green-600 text-white rounded-md hover:bg-green-700 transition-colors disabled:bg-gray-400"
+        >
+          <FiCheckCircle className="-ml-0.5 h-5 w-5" />
+          Approve
+        </button>
+      </div>
+    </div>
+  );
+
+  const handleSearchFilter = (term) => {
+    setSearchValue(term);
+  };
+
+  const handleResetSearchBy = () => {
+    dispatch(getRepayments({ pageNumber: 0, pageSize: 20 }));
+  };
+
+  const handleFullLoanDetails = async (loanId, uid) => {
+    setShowLoanModal(true);
+    await dispatch(getFullLoanDetails({ loanId, uid })).unwrap();
+  };
+
+  const closeFullLoanDetailModal = () => {
+    setShowLoanModal(false);
+  };
+
+  const searchOptions = [
+    { label: "User Id", value: "userId" },
+    { label: "Loan Id", value: "loan" },
+    { label: "Collection By", value: "collectionBy" },
+    { label: "Accounting", value: "accounting" },
+    { label: "Method", value: "method" },
+    { label: "Transaction Id", value: "transactionId" },
+    { label: "Installment Id", value: "installmentId" },
+    { label: "Request Id", value: "requestId" },
+  ];
+
+  const SearchRepaymentByFieldSearch = () => {
+    dispatch(fetchRepaymentByField({ field: searchBy, value: searchValue }));
+    setSearchBy("");
+    setSearchValue("");
+  };
+
+  return (
+    <div className={`flex flex-col gap-3`}>
+      <ContainerTile className={`flex justify-between gap-5 align-middle`}>
+        <div className="w-[45%]">
+          <InputSelect
+            labelName="Search By"
+            inputName="searchBy"
+            inputOptions={searchOptions}
+            inputValue={searchBy}
+            onChange={(e) => setSearchBy(e.target.value)}
+          />
+        </div>
+        <div className="w-[45%]">
+          <InputText
+            labelName="Enter Value"
+            inputName="searchValue"
+            inputValue={searchValue}
+            onChange={(e) => handleSearchFilter(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="flex align-middle gap-5">
+          <Button
+            buttonName={"Search"}
+            onClick={SearchRepaymentByFieldSearch}
+            rectangle={true}
+            className={`mt-4 h-fit self-center`}
+          />
+          <Button
+            buttonName={"Reset"}
+            onClick={handleResetSearchBy}
+            rectangle={true}
+            className={`mt-4 h-fit self-center`}
+          />
+        </div>
+      </ContainerTile>
+
+      <ExpandableTable
+        columns={ApproveRepaymentColumns}
+        data={transformedRepaymentData}
+        renderExpandedRow={renderExpandedRow}
+        loading={loading}
+        error={error}
+      />
+      <Pagination
+        totalElements={approveRepaymentTotalElements}
+        dispatcherFunction={dispatcherFunction}
+        pageSize={pageSize}
+      />
+      <FullLoanDetailModal
+        isOpen={showLoanModal}
+        onClose={closeFullLoanDetailModal}
+        loanDetails={fullLoanDetails}
+        loading={loading2}
+      />
+    </div>
+  );
+};
+
+export default ApproveRepaymentTest;
