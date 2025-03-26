02880369c092b79b404fb1ebad07c57d9a5edc5b
dm-lalwani <mr.d.m.lalwani@gmail.com>
2025-03-26 15:37:42 +0530

    react, tailwind & other dependecies updated and AppLayout updated as per new theme

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -4,23 +4,24 @@ import Header from "../Common/Header/Header";
 import Footer from "../Common/Footer/Footer";
 import SideBar from "../Common/Sidebar/Sidebar";
 import Body from "../Common/Body/Body";
-import LoadingState from '../LoadingState/LoadingState';
-
+import LoadingState from "../LoadingState/LoadingState";
 
 const AppLayout = () => {
   return (
-    <div className="flex flex-col max-h-screen">
-      <Header />
-      <div className="flex flex-1 overflow-hidden">
-        <SideBar />
-        <div className="flex flex-col flex-1 overflow-y-auto">
-          <Suspense fallback={<LoadingState />}>
-            <Body>
-              <Outlet />
-            </Body>
-          </Suspense>
-          <Footer />
-        </div>
+    <div className="flex h-[100dvh] overflow-hidden">
+      <SideBar />
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+        <Header />
+        <main className="grow">
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-[96rem] mx-auto">
+            <Suspense fallback={<LoadingState />}>
+              <Body>
+                <Outlet />
+              </Body>
+            </Suspense>
+          </div>
+        </main>
+        <Footer />
       </div>
     </div>
   );
